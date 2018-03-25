f002d29fdcd9a019c9f4dbf18a0e076763b1e95e
unknown <mukarrambinsaeed@gmail.com>
2018-03-26 00:38:00 +0500

    built star wars movie app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movies from './Movies/Movies';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        My First React Git Project!
+        <h1>Star Wars Movies List</h1>
+        <Movies />
       </div>
     );
   }
